11853ff43dd8eac89ed16314b522357221a321bc
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-01-08 15:33:59 +0900

    最新記事一覧にタイトルと日付(※path)を表示 #21

--- docs/.vuepress/theme/global-components/LatestArticleList.vue
+++ docs/.vuepress/theme/global-components/LatestArticleList.vue
@@ -2,7 +2,10 @@
   <div class="bg-green-100">
     <ul>
       <li v-for="article in latest_articles" :key="article.path">
-        <a :href="article.path">{{ article.path }}</a>
+        <div>
+          <div>{{ article.path }}</div>
+          <a :href="article.path">{{ articles_map[article.key]["title"] }}</a>
+        </div>
       </li>
     </ul>
   </div>
@@ -10,7 +13,7 @@
 
 <script lang="ts">
 import latest_articles from "../../../../generated/dynamic/latest_articles.js"
-import articles_map from "../../../../generated/dynamic/latest_articles.js"
+import articles_map from "../../../../generated/dynamic/articles_map.js"
 export default {
   data() {
     return {
